0109ce505abc5bc98ad5346eccd8618e0463f7ee
jaylin0131 <jaylego0810@gmail.com>
2025-01-20 13:50:50 +0800

    About css edited

---
+++ static/js/Home.js
@@ -0,0 +1,9 @@
+document.addEventListener('scroll', function () {
+    const aboutSection = document.getElementById('about');
+    const position = aboutSection.getBoundingClientRect();
+  
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+      aboutSection.classList.add('visible');
+    }
+  });
+  
