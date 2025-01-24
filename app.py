e40927b6b3aa0ec5446c65742ff52605a24c8496
jaylin0131 <jaylego0810@gmail.com>
2025-01-25 00:58:13 +0800

    login system added, not FINISH

--- app.py
+++ app.py
@@ -1,15 +1,65 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, config, flash, redirect, render_template, request, send_from_directory, url_for
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+# app.secret_key = 'frc7636'
 
-# 根路徑，渲染 HTML 模板
-@app.route('/')
-def home():
-    return render_template('home.html')
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.session_protection = 'strong'
+# login_manager.login_view = 'login'
+# login_manager.login_message = '請先登入'
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+# users = {'admin': {'password': 'frc7636'}}
+
+# class User(UserMixin):
+#     pass
+
+# @login_manager.user_loader
+# def user_loader(user_role):
+#     if user_role != 'admin':
+#         return
+#     user = User()
+#     user.id = user_role
+#     return user
+
+# @login_manager.request_loader
+# def request_loader(request):
+#     user_role = request.form.get('user_role')
+#     if user_role != 'admin':
+#         return
+#     user = User()
+#     user.id = user_role
+
+#     user.is_authenticated = request.form['password'] == users[user_role]["password"]
+#     return user
+
+# # 根路徑，渲染 HTML 模板
+# @app.route('/')
+# def home():
+#     return render_template('home.html')
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'GET':
+#         return render_template('login.html')
+    
+#     user_role = request.form['user_role']
+#     if (user_role in users) and (request.form['password'] == users[user_role]['password']):
+#         user = User()
+#         user.id = user_role
+#         login_user(user)
+#         flash('登入成功')
+#         return redirect(url_for('home'))
+    
+#     flash('登入失敗')
+#     return render_template('login.html')
+
+# @app.route('/logout')
+# def logout():
+#     logout_user()
+#     flash('登出成功')
+#     return render_template('home.html')
 
 # 用來提供靜態檔案
 @app.route('/static/<path:filename>')
