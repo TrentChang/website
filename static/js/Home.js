18268e621c8f2cb0507710c57ed0be005832f077
TrentChang <chienchuanchang@gmail.com>
2025-01-24 08:51:22 +0800

    three.js test (failed

--- static/js/Home.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("scroll", function () {
-  const aboutSection = document.getElementById("about");
-  const position = aboutSection.getBoundingClientRect();
-
-  if (position.top < window.innerHeight && position.bottom >= 0) {
-    aboutSection.classList.add("visible");
-  }
-});
